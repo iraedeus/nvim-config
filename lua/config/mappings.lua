cd2984f07c2a134a51f4aed778d5eaff6d5cc204
iraedeus <dtotjmyanin@mail.ru>
2025-07-21 08:04:27 +0300

    Initial commit

---
+++ lua/config/mappings.lua
@@ -0,0 +1,29 @@
+-- Переключение между окнами
+vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Move to left window', remap = true })
+vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Move to lower window', remap = true })
+vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Move to upper window', remap = true })
+vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Move to right window', remap = true })
+
+-- Выход из normal режима
+vim.keymap.set('i', 'jk', '<Esc>', { noremap = true, silent = true })
+
+-- Сохранение файла
+vim.keymap.set('n', '<leader>w', ':w<CR>', { desc = 'Save file' })
+
+-- Neotree
+vim.keymap.set('n', '<leader>n', ':Neotree toggle<CR>', { desc = 'Neotree open file manager' })
+vim.keymap.set('n', '<leader>b', ':Neotree buffers toggle<CR>', { desc = 'Neotree open buffers' })
+
+-- Telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+vim.keymap.set('n', '<leader>gs', builtin.git_status, { desc = 'Telescope git status' })
+vim.keymap.set('n', '<leader>gc', builtin.git_commits, { desc = 'Telescope git commits' })
+vim.keymap.set('n', '<leader>gb', builtin.git_branches, { desc = 'Telescope git branches' })
+vim.keymap.set('n', '<leader>gr', builtin.lsp_references, { desc = 'Telescope references' })
+vim.keymap.set('n', '<leader>gd', builtin.lsp_definitions, { desc = 'Telescope go to definition' })
+
+
---
+++ lua/options.lua
@@ -0,0 +1,39 @@
+local opt = vim.opt
+
+vim.o.termguicolors = true
+
+vim.g.mapleader = ' '
+vim.g.maplocleader = '\\'
+
+-- Файлы и история изменений
+opt.swapfile = false
+opt.backup = false
+local undodir = vim.fn.stdpath('data') .. '/undodir'
+if vim.fn.isdirectory(undodir) == 0 then
+  vim.fn.mkdir(undodir, 'p')
+end
+opt.undodir = undodir
+opt.undofile = true
+
+-- Интерфейс
+opt.number = true
+opt.relativenumber = true
+opt.cursorline = true
+opt.termguicolors = true
+opt.signcolumn = 'yes'
+opt.scrolloff = 8
+opt.sidescrolloff = 8
+opt.cmdheight = 2
+opt.list = true
+opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+opt.wrap = false
+
+-- Отступы
+opt.expandtab = true
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.smartindent = true
+
+-- Отключение мыши
+opt.mouse = ''
