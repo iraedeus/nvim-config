a025965bb2af531015c1eb8f99430a1c875553fa
iraedeus <dtotjmyanin@mail.ru>
2025-07-23 00:40:39 +0300

    feat: add wrap in tex files

--- lua/options.lua
+++ lua/options.lua
@@ -37,3 +37,14 @@ opt.smartindent = true
 
 -- Отключение мыши
 opt.mouse = ''
+
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+    pattern = { "markdown", "txt", "tex", "gitcommit" },
+    callback = function()
+        vim.opt_local.wrap = true
+        vim.opt_local.linebreak = true
+    end,
+    desc = "Enable wrap for text-based filetypes",
+})
+opt.wrap = false
