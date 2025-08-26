466e441dd1cdea61f57fa54a51e3228e94247c2b
iraedeus <dtotjmyanin@mail.ru>
2025-08-26 14:29:08 +0300

    feat: dap setup for pytest files

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -19,6 +19,20 @@ return {
                     dap.listeners.before.event_exited["dapui_config"] = function()
                         dapui.close()
                     end
+
+                    dap.configurations.python = {
+                        {
+                            type = 'python', -- Используем адаптер для Python
+                            request = 'launch',
+                            name = "Debug: Pytest File",
+                            -- Ключевой момент: мы запускаем pytest как модуль
+                            module = "pytest",
+                            -- Аргументы для pytest, например, для вывода print() и подробного лога
+                            args = { "-s", "-v", "${file}" },
+                            -- Запускать в интегрированном терминале, чтобы видеть вывод
+                            console = "integratedTerminal",
+                        },
+                    }
                 end
             },
             {
