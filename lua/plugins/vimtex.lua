2d4dacc6162eceddbb566d71cbfaafbc2ec21787
iraedeus <dtotjmyanin@mail.ru>
2025-12-18 03:40:13 +0300

    feat: add spellchecker for en

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -10,6 +10,14 @@ return {
                 continuous = 1,
                 callback = 1,
             }
+
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = "tex",
+                callback = function()
+                    vim.opt_local.spell = true
+                    vim.opt_local.spelllang = "en_us" -- или "en_gb" для британского
+                end,
+            })
         end
     }
 }
