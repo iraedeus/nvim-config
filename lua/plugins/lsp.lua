4d24ad0647cde5c435c8a2af585b8ba7588d6877
Danil Totmyanin <144918156+iraedeus@users.noreply.github.com>
2026-02-21 15:08:59 +0300

    Python/setup (#3)
    * feat: setup python
    
    * feat: add dap
    
    * feat: dap setup for pytest files
    
    * feat: add variable renaming

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,15 +3,37 @@ return {
         'neovim/nvim-lspconfig',
         config = function()
             local lspconfig = require('lspconfig')
+
+            local on_attach = function(client, bufnr)
+                local opts = { buffer = bufnr, remap = false }
+
+                vim.keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts)
+
+                vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)
+                vim.keymap.set("n", "gr", "<cmd>Lspsaga finder<CR>", opts)
+            end
+
             vim.diagnostic.config({
                 virtual_text = true
             })
 
             lspconfig.lua_ls.setup({
+                on_attach = on_attach,
                 settings = {
                     Lua = {
                         diagnostics = {
                             globals = { 'vim' }
+                        }
+                    }
+                }
+            })
+
+            lspconfig.pyright.setup({
+                on_attach = on_attach,
+                settings = {
+                    python = {
+                        analysis = {
+                            diagnosticMode = "workspace",
                         }
                     }
                 }
