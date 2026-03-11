d68490639141e94c2c11e9b2fcae58f3fbef52ad
iraedeus <dtotjmyanin@mail.ru>
2026-03-11 12:56:47 +0300

    feat: add golang

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -32,6 +32,10 @@ return {
 
             lspconfig.texlab.setup({})
 
+            -- ######
+            -- PYTHON
+            -- ######
+
             lspconfig.pyright.setup({
                 on_attach = on_attach,
                 settings = {
@@ -41,6 +45,41 @@ return {
                         }
                     }
                 }
+            })
+
+            -- ######
+            -- GOLANG
+            -- ######
+
+            lspconfig.gopls.setup({
+                on_attach = on_attach,
+                settings = {
+                    gopls = {
+                        analyses = {
+                            unusedparams = true,
+                        },
+                        staticcheck = true,
+                        gofumpt = true,
+                    },
+                },
+            })
+
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                pattern = "*.go",
+                callback = function()
+                    local params = vim.lsp.util.make_range_params()
+                    params.context = { only = { "source.organizeImports" } }
+                    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 1000)
+                    for cid, res in pairs(result or {}) do
+                        for _, r in pairs(res.result or {}) do
+                            if r.edit then
+                                local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+                                vim.lsp.util.apply_workspace_edit(r.edit, enc)
+                            end
+                        end
+                    end
+                    vim.lsp.buf.format({ async = false })
+                end
             })
         end,
     },
